5cb73a394715e4c9e6a5c34225d946f3d6ed4a52
sweezy793 <sarthaks793@gmail.com>
2018-12-19 01:25:22 +0530

    Authentication

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const passport=require('passport')
+const cookieSession=require('cookie-session')
 const authRoutes=require('./routes/authRoutes')
 const keys=require('./config/keys')
 require('./models/user')
@@ -9,7 +11,15 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
 
 const app=express();
 
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+)
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,17 @@ const keys=require('../config/keys')
 
 const User=mongoose.model('users');   //just a different way of requiring userschema because conventional way might cause error
 
+passport.serializeUser((user,done)=>{
+	done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+	User.findById(id)
+	.then((user)=>{
+		done(null,user);
+	})
+})
+
 passport.use(new GoogleStrategy({
         clientID:keys.googleClientID,
         clientSecret:keys.googleClientSecret,
@@ -21,6 +32,12 @@ passport.use(new GoogleStrategy({
 					.save()
 					.then(user=>{done(null,user)})
                 }       
-        })    
+        })
     })
 );
+
+
+
+
+
+
